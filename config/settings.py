55b5f37ccb00090bcc3d9955139fe0f3cf08a5bd
irsaduberia <irsaduberia@gmail.com>
2026-05-20 01:31:47 +0530

    fix media storage with cloudinary

--- config/settings.py
+++ config/settings.py
@@ -26,18 +26,6 @@ INSTALLED_APPS = [
 
 ]
 
-# MIDDLEWARE
-
-MIDDLEWARE = [
-'django.middleware.security.SecurityMiddleware',
-'django.contrib.sessions.middleware.SessionMiddleware',
-'django.middleware.common.CommonMiddleware',
-'django.middleware.csrf.CsrfViewMiddleware',
-'django.contrib.auth.middleware.AuthenticationMiddleware',
-'django.contrib.messages.middleware.MessageMiddleware',
-'django.middleware.clickjacking.XFrameOptionsMiddleware',
-]
-
 # URLS
 
 ROOT_URLCONF = 'config.urls'
@@ -127,3 +115,10 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+INSTALLED_APPS += [
+    'cloudinary',
+    'cloudinary_storage',
+]
+
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
