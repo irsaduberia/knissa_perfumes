ca414ec1a67ab953ed0923ed67de3e3741928a9f
irsaduberia <irsaduberia@gmail.com>
2026-05-20 03:13:41 +0530

    fix settings + cloudinary media storage

--- config/settings.py
+++ config/settings.py
@@ -1,113 +1,41 @@
 from pathlib import Path
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# -----------------------
 # SECURITY
-
+# -----------------------
 SECRET_KEY = 'django-insecure-!6r^8c8+pp+=udsux95=!wddvwhg5mqs#g$()+o-lw%2tv-%2@'
 DEBUG = True
-ALLOWED_HOSTS = []
-
-# APPLICATIONS
-
-INSTALLED_APPS = [
-'django.contrib.admin',
-'django.contrib.auth',
-'django.contrib.contenttypes',
-'django.contrib.sessions',
-'django.contrib.messages',
-'django.contrib.staticfiles',
-
-
-'store',
-
-
-]
-
-# URLS
-
-ROOT_URLCONF = 'config.urls'
-
-# TEMPLATES
-
-TEMPLATES = [
-{
-'BACKEND': 'django.template.backends.django.DjangoTemplates',
-'DIRS': [],  # you can add BASE_DIR / 'templates' later if needed
-'APP_DIRS': True,
-'OPTIONS': {
-'context_processors': ['store.context_processors.global_data',
-'django.template.context_processors.request',
-'django.contrib.auth.context_processors.auth',
-'django.contrib.messages.context_processors.messages',
-],
-},
-},
-]
-
-# WSGI
-
-WSGI_APPLICATION = 'config.wsgi.application'
-
-# DATABASE
-
-DATABASES = {
-'default': {
-'ENGINE': 'django.db.backends.sqlite3',
-'NAME': BASE_DIR / 'db.sqlite3',
-}
-}
-
-# PASSWORD VALIDATION
-
-AUTH_PASSWORD_VALIDATORS = [
-{
-'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-},
-{
-'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-},
-{
-'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-},
-{
-'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-},
-]
-
-# INTERNATIONALIZATION
-
-LANGUAGE_CODE = 'en-us'
-TIME_ZONE = 'UTC'
-USE_I18N = True
-USE_TZ = True
-
-# STATIC FILES (CSS, JS)
-
-STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / "staticfiles"
-
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
-
-STATICFILES_DIRS = [
-BASE_DIR / "static",
-]
-
-
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
-
-
-
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 ALLOWED_HOSTS = ["*"]
 
+# -----------------------
+# APPLICATIONS
+# -----------------------
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+
+    # third-party
+    'cloudinary',
+    'cloudinary_storage',
+
+    # local app
+    'store',
+]
+
+# -----------------------
+# MIDDLEWARE
+# -----------------------
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
+
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -116,9 +44,83 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-INSTALLED_APPS += [
-    'cloudinary',
-    'cloudinary_storage',
+# -----------------------
+# URLS
+# -----------------------
+ROOT_URLCONF = 'config.urls'
+
+# -----------------------
+# TEMPLATES
+# -----------------------
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'store.context_processors.global_data',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
 ]
 
+# -----------------------
+# WSGI
+# -----------------------
+WSGI_APPLICATION = 'config.wsgi.application'
+
+# -----------------------
+# DATABASE (SQLite for now)
+# -----------------------
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+# -----------------------
+# PASSWORD VALIDATION
+# -----------------------
+AUTH_PASSWORD_VALIDATORS = [
+    {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'},
+]
+
+# -----------------------
+# INTERNATIONALIZATION
+# -----------------------
+LANGUAGE_CODE = 'en-us'
+TIME_ZONE = 'UTC'
+USE_I18N = True
+USE_TZ = True
+
+# -----------------------
+# STATIC FILES
+# -----------------------
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
+# -----------------------
+# MEDIA (handled by Cloudinary)
+# -----------------------
+MEDIA_URL = '/media/'
+
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+
+# -----------------------
+# DEFAULT AUTO FIELD
+# -----------------------
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
